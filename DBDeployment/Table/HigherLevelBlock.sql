5f56625059b1961731f02e37fba9a73d1d8bfbd7
luckyyd <luckyyd@tju.edu.cn>
2015-11-20 15:09:40 +0800

    1. 有一个文件没有commit

---
+++ DBDeployment/Table/HigherLevelBlock.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[HigherLevelBlock]
+(
+	[Hash] VARCHAR(1024) NOT NULL PRIMARY KEY, 
+    [blockSize] INT NOT NULL, 
+    [blockHeader] VARCHAR(MAX) NOT NULL, 
+    [LowerLevelBlockCounter] INT NOT NULL, 
+    [LowerLevelBlockSet] VARCHAR(MAX) NOT NULL, 
+    [Period] INT NOT NULL, 
+    [Signature] VARCHAR(1024) NOT NULL
+)
